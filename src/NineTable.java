fd414d63b7a6e74018c05b421c55b9def16c8bbe
anil1011 <mylifebth@gmail.com>
2025-02-03 15:15:00 +0530

    second commit

---
+++ src/NineTable.java
@@ -0,0 +1,9 @@
+public class NineTable {
+    public static void main(String[] args) {
+        int number = 9;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
+
